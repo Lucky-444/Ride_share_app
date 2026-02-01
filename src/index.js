bca4849e995143806f6ac6e04868f22547a40b45
Lucky <sashankswain6@gmail.com>
2026-02-02 01:16:09 +0530

    Basic SetUp Done

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config();
+const app = express();
+
+const PORT = process.env.PORT;
+
+app.get('/' , (req , res) => {
+         res.send("Home Page")
+})
+app.listen(PORT ,() =>{
+         console.log(`Server running On Port ${PORT}`);
+})
