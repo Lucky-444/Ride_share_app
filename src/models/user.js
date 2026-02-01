0ce271a02c6e8520778f5877343370ae8714fe7e
Lucky <sashankswain6@gmail.com>
2026-02-02 01:24:20 +0530

    Add : user schema

---
+++ src/models/user.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+const userSchema = new mongoose.Schema(
+  {
+    fullname: {
+      firstname: {
+        type: String,
+        required: true,
+        minlength: 3,
+      },
+      lastname: {
+        type: String,
+        minlength: 3,
+      },
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 8,
+      select: false,
+    },
+    phone: {
+      type: String,
+      minlength: 10,
+      maxlength: 10,
+    },
+    socketId: {
+      type: String,
+    },
+  },
+  { timestamps: true },
+);
+
+userSchema.statics.hashPassword = async function (password) {
+  return await bcrypt.hash(password, 10);
+};
+
+userSchema.methods.comparePassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+module.exports = mongoose.model("User", userSchema);
