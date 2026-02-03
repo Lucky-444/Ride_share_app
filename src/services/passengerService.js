ca63eb403c78636e1c4d9067eeea989ff63f20a2
Lucky <sashankswain6@gmail.com>
2026-02-04 03:30:09 +0530

    Add : Passenger Logic

--- src/services/passengerService.js
+++ src/services/passengerService.js
@@ -1,6 +1,18 @@
 const bookingRepository = require("../repositories/bookingRepository");
+const findPassengerById = require("../repositories/passengerRepository");
+const GetPassengerService = async (passengerId) => {
+  try {
+    const passengerDetails = findPassengerById(passengerId);
 
-const GetPassengerService = async (passengerId) => {};
+    if (!passengerDetails) {
+      throw new Error("Passenger Not Found");
+    }
+
+    return passengerDetails;
+  } catch (error) {
+    throw new Error("Something Happened");
+  }
+};
 
 const FeedBackService = async (passengerId, bookingId, rating, feedback) => {
   const Booking = await bookingRepository.findBooking({
@@ -8,15 +20,14 @@ const FeedBackService = async (passengerId, bookingId, rating, feedback) => {
     passenger: passengerId,
   });
 
-  if(!Booking){
-         throw new Error("booking Not Found");
+  if (!Booking) {
+    throw new Error("booking Not Found");
   }
 
   Booking.rating = rating;
   Booking.feedback = feedback;
 
   await Booking.save();
-
 };
 
 module.exports = {
